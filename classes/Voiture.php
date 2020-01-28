808a798462770eac59721c8799b595c9c6fb204c
Axel Barbé--Husson <47109555+AxelVitch@users.noreply.github.com>
2020-01-28 11:36:37 +0100

    class voiture -def prop

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -9,4 +9,19 @@ class Voiture
     public $dimension = [0, 0, 0];
     public $masse;
     public $vitesse = 0;
+
+    public function affichermsg()
+    {
+        echo "Je suis Michel";
+    }
+
+    public function __construct(string $mar, string $mod, string $cou, array $dim, int $mas)
+    {
+        $this->marque = $mar;
+        $this->modele = $mod;
+        $this->couleur = $cou;
+        $this->dimension = $dim;
+        $this->masse = $mas;
+    }
+
 }
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require ('./classes/Voiture.php');
 require ('./functions/debug.php');
-$voiture1 = new Voiture();
+
+$voiture1 = new Voiture("Lada", "Niva","Blanc",[2000,1500,500],500);
 debug($voiture1);
