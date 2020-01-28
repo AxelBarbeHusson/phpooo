e8d15de3fdd672325ed92e87a1ab64bb8be153ce
Axel Barbé--Husson <47109555+AxelVitch@users.noreply.github.com>
2020-01-28 16:58:07 +0100

    form object

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
-require_once './classes/Vehicule.php';
-require_once './classes/Voiture.php';
-require_once './classes/Coupe.php';
-require './functions/debug.php';
-$toto = new Coupe("Peugeot", "504", "vert", [2000, 1000, 1000], 1200);
-debug($toto);
+date_default_timezone_set('Europe/Paris');
+require_once './functions/classAutoLoader.php';
+spl_autoload_register('classAutoLoader');
 
-$voiture1 = new Voiture("Lada", "Niva", "blanc", [2000, 1500, 800], 900);
-$voiture2 = new Voiture("Nissan", "Cube", "violet", [2500, 1800, 1200], 1200);
-
-echo $voiture1->getMarque();
-
-
-$voiture1->vitesse = 25;
-$voiture2->vitesse = 30;
+$formulaire = new Form('#','frmContact');
+echo $formulaire->displayForm();
